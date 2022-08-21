bfa7ae3b1ea33572408d5944190483cba9e7dae8
dannywarekuromo <dannywarekuromo@gmail.com>
2022-08-21 15:21:20 +0100

    style sheet reconfiguration

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -5,8 +5,6 @@ const MovieCard = ({movie}) => {
         <div>
             <div className='movie'>
                 <p>{movie.Year}</p>
-            </div>
-            <div>
                 <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title}/>
             </div>
             <div>
