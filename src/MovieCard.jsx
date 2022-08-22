709b349e3533020fd24f88322be66bc114d7b942
dannywarekuromo <dannywarekuromo@gmail.com>
2022-08-22 14:33:42 +0100

    style sheet clean up

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -7,9 +7,9 @@ const MovieCard = ({movie}) => {
                 <p>{movie.Year}</p>
                 <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title}/>
             </div>
-            <div>
-                <span>{movie.Type}</span>
+            <div className='movie-details'>
                 <h3>{movie.Title}</h3>
+                <span>{movie.Type}</span>
             </div>
         </div>
     )
